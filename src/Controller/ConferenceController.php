f1416cde9fb9349cd6b309543c7bd923ca043b77
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2026-01-28 15:14:34 +0100

    Display entities

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,15 +3,34 @@
 namespace App\Controller;
 
 use App\Entity\Conference;
+use App\Repository\ConferenceRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\Requirement\Requirement;
 
+#[Route('/conference')]
 class ConferenceController extends AbstractController
 {
-    #[Route('/conference/{name}/{start}/{end}', name: 'app_conference_new', requirements: ['name' => Requirement::ASCII_SLUG, 'start' => Requirement::DATE_YMD, 'end' => Requirement::DATE_YMD])]
+    #[Route('', name: 'app_conference_list', methods: ['GET'])]
+    public function list(ConferenceRepository $repository): Response
+    {
+        return $this->render('conference/list.html.twig', [
+            'conferences' => $repository->findAll(),
+        ]);
+    }
+
+    #[Route('/{id<\d+>}', name: 'app_conference_show', methods: ['GET'])]
+    public function show(Conference $conference): Response
+    {
+        return $this->render('conference/show.html.twig', [
+            'conference' => $conference,
+        ]);
+    }
+
+    #[Route('/{name}/{start}/{end}', name: 'app_conference_new', requirements: ['name' => Requirement::ASCII_SLUG, 'start' => Requirement::DATE_YMD, 'end' => Requirement::DATE_YMD])]
     public function newConference(string $name, string $start, string $end, EntityManagerInterface $manager): Response
     {
         $conference = (new Conference())
--- src/Repository/ConferenceRepository.php
+++ src/Repository/ConferenceRepository.php
@@ -16,6 +16,30 @@ class ConferenceRepository extends ServiceEntityRepository
         parent::__construct($registry, Conference::class);
     }
 
+    /**
+     * @return Conference[]
+     */
+    public function findConferencesBetweenDates(?\DateTimeImmutable $start = null, ?\DateTimeImmutable $end = null): array
+    {
+        if (null === $start && null === $end) {
+            throw new \InvalidArgumentException('At least one date is required to operate this method.');
+        }
+
+        $qb = $this->createQueryBuilder('c');
+
+        if ($start instanceof \DateTimeImmutable) {
+            $qb->andWhere('c.startAt >= :start')
+                ->setParameter('start', $start);
+        }
+
+        if ($end instanceof \DateTimeImmutable) {
+            $qb->andWhere('c.endAt <= :end')
+                ->setParameter('end', $end);
+        }
+
+        return $qb->getQuery()->getResult();
+    }
+
     //    /**
     //     * @return Conference[] Returns an array of Conference objects
     //     */
