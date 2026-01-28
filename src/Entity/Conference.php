3f7e9c44c9f74f593bc72432cab39318b84a5d11
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2026-01-28 17:29:16 +0100

    Security

--- src/Entity/Conference.php
+++ src/Entity/Conference.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ConferenceRepository::class)]
 class Conference
@@ -16,21 +17,31 @@ class Conference
     #[ORM\Column]
     private ?int $id = null;
 
+    #[Assert\Length(min: 10)]
+    #[Assert\NotBlank()]
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
+    #[Assert\Length(min: 30)]
+    #[Assert\NotBlank()]
     #[ORM\Column(type: Types::TEXT)]
     private ?string $description = null;
 
+    #[Assert\NotNull()]
     #[ORM\Column]
     private ?bool $accessible = null;
 
+    #[Assert\Length(min: 20)]
     #[ORM\Column(type: Types::TEXT, nullable: true)]
     private ?string $prerequisites = null;
 
+    #[Assert\GreaterThanOrEqual('now')]
+    #[Assert\NotBlank()]
     #[ORM\Column]
     private ?\DateTimeImmutable $startAt = null;
 
+    #[Assert\GreaterThanOrEqual(propertyPath: 'startAt')]
+    #[Assert\NotBlank()]
     #[ORM\Column]
     private ?\DateTimeImmutable $endAt = null;
 
@@ -43,6 +54,8 @@ class Conference
     /**
      * @var Collection<int, Organization>
      */
+    #[Assert\NotBlank()]
+    #[Assert\Valid]
     #[ORM\ManyToMany(targetEntity: Organization::class, mappedBy: 'conferences')]
     private Collection $organizations;
 
