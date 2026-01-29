dbc5f2d081a30e98f443decdd0357690cb3295c4
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2026-01-29 10:34:33 +0100

    Exercice 1 - ConferenceSearch

--- src/Repository/ConferenceRepository.php
+++ src/Repository/ConferenceRepository.php
@@ -40,6 +40,16 @@ class ConferenceRepository extends ServiceEntityRepository
         return $qb->getQuery()->getResult();
     }
 
+    public function findLikeName(string $name): array
+    {
+        $qb = $this->createQueryBuilder('c');
+
+        return $qb->andWhere($qb->expr()->like('c.name', ':name'))
+            ->setParameter('name', '%'.$name.'%')
+            ->getQuery()
+            ->getResult();
+    }
+
     //    /**
     //     * @return Conference[] Returns an array of Conference objects
     //     */
